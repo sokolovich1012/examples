f327bf281e393fb0797900ef8d3347f9e2bb6974
sokolovich1012 <sokolovich1.99@mail.ru>
2023-01-25 19:42:11 +0300

    add 005

---
+++ example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+if(username == "Alesa")
+{
+ Console.WriteLine("Bro");
+}
+else 
+{
+ Console.Write("привет,");
+ Console.WriteLine(username);
+}
