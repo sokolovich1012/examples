f79568e1df469bcd00d09fc508a0366b00fede6b
sokolovich1012 <sokolovich1.99@mail.ru>
2023-01-25 18:44:02 +0300

    add namber

--- example004/Program.cs
+++ example004/Program.cs
@@ -1,3 +1,6 @@
-﻿double namberA=(12);
-double namberB=(5);
-Console.WriteLine(namberA/namberB);
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = (numberA+numberB);
+Console.WriteLine(result);
