52b36a158cbb593346cbce4445ef112b4601f316
sokolovich1012 <sokolovich1.99@mail.ru>
2023-01-26 20:53:20 +0300

    add Class01

---
+++ Class01/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear ();
+Console.Write("take number");
+int number = int.Parse(Console.ReadLine());
+int sqw = number * number;
+Console.WriteLine("квадрат числа" + number + "равен="+sqw);
